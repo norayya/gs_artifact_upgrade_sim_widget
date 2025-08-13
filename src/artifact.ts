c81bec33ad226001205ba38d3045b4013ebeeb59
norara <792167485@qq.com>
2025-08-13 12:43:15 +0800

    update

--- src/artifact.ts
+++ src/artifact.ts
@@ -1,169 +1,153 @@
-import {MainStat, Stat, SubStat} from "./stat";
-import {get_random_int, ruletka} from "./utils";
+import { GetRandom, Ruletka, ValueType} from "./utils";
 import {Event} from "./event";
+import {ArtifactType, Stat} from "./types";
+import {GetNewSubStat, GetRandomMainStatByArtifactType} from "./stats_data";
 
-
-/**
- * 副词条
- */
-export type SubStatObject = { sub_stat: Stat, upgrade_ranks: number[] }
-
-/**
- * 圣遗物
- */
-export class Artifact {
-    private main: Stat;
-    private sub: SubStatObject[];
-    private level: number;
-    private type: ArtifactType;
-
-    /**
-     * 添加词条事件
-     * @template T {sub_stat:Stat, rank: number} , sub_stat是被添加的词条类型， rank是初始化词条的强化档位
-     */
-    public on_new_sub_stat_added: Event<{sub_stat: Stat, rank: number}>;
-
-    /**
-     * 词条被强化事件
-     * @template T {sub_stat_idx:number， rank: number}, sub_stat_idx是被强化的词条是圣遗物的哪条词条, rank是这次强化随机到的强化档位
-     */
-    public on_sub_stat_upgraded: Event<{sub_stat_idx: number, rank: number}>;
-
-    /**
-     * 创建一个圣遗物
-     * @param {ArtifactType} artifact_type 圣遗物部位
-     * @param {(artifact: Artifact) => void} opts 配置选项
-     */
-    constructor(artifact_type: ArtifactType, opts?:(artifact: Artifact) => void) {
-        // 初始强化等级 0
-        this.level = 0;
-
-        // 类型
-        this.type = artifact_type;
-
-        // 初始化事件订阅器
-        this.on_new_sub_stat_added = new Event<{sub_stat: Stat, rank: number}>();
-        this.on_sub_stat_upgraded = new Event<{sub_stat_idx: number, rank: number}>();
-
-        // 配置opts
-        if(opts !== undefined){
-            opts(this);
-        }
-
-
-        // 根据传入的部位确定主词条
-        this.main = Artifact.get_random_main_stat_by_artifact_type(artifact_type);
-        this.sub = [];
-
-        // 开始添加初始的副词条
-        // 随机决定初始副词条的数量
-        const c = get_random_int(0, 2000) % 2 === 0 ? 3 : 4;
-
-        for(let i = 0; i < c; i++) {
-            /*
-             level_up函数也使用了这部分代码,
-             如果修改了这里, 那么也应该修改level_up函数
-             */
-
-            // 已有词条集合
-            const subs: Stat[] = [];
-            this.sub.forEach(x => {
-                subs.push(x.sub_stat);
-            });
-
-            // 随机获得新的词条
-            const new_stat = SubStat.get_new_sub_stat([this.main, ...subs]);
-            // 为词条选择一个初始强化档位
-            const upgrade_rank = get_random_int(0, 4);
-
-            // 插入词条
-            this.sub.push({
-                sub_stat: new_stat,
-                upgrade_ranks: [upgrade_rank]
-            });
-
-            // 通知事件
-            this.on_new_sub_stat_added.emit({sub_stat: new_stat, rank: upgrade_rank});
-
-        }
-
-    }
-
-
-
-    /**
-     * 强化一级
-     */
-    public level_up(): void {
-        // 强化一级
-        if(this.level < 0 || this.level >= 20)
-        {
-            throw new Error("当前圣遗物等级不能再被强化")
-        }
-
-        this.level ++ ;
-
-        // 如果当前强化等于4 8 12 16 20时,
-        // 当前副词条数量少于4个时, 添加一个随机词条
-
-        if(this.level == 4 || this.level == 8 || this.level == 12 || this.level == 16 || this.level == 20){
-            if(this.sub.length < 4 ){
-                /*
-                 这里就是复制粘贴了构造函数中添加词条的部分
-                 似乎没什么必要抽出来一个独立函数
-                 如果改动过构造函数, 请务必也改动这里
-                 */
-
-                // 已有词条集合
-                const subs: Stat[] = [];
-                this.sub.forEach(x => {
-                    subs.push(x.sub_stat);
-                });
-                // 随机获得新的词条
-                const new_stat = SubStat.get_new_sub_stat([this.main, ...subs]);
-                // 为词条选择一个初始强化档位
-                const upgrade_rank = get_random_int(0, 4);
-
-                // 插入词条
-                this.sub.push({
-                    sub_stat: new_stat,
-                    upgrade_ranks: [upgrade_rank]
-                });
-
-                // 通知事件
-                this.on_new_sub_stat_added.emit({sub_stat: new_stat, rank: upgrade_rank});
-
-            } else {
-                // 如果已经有4个词条了,
-                // 那么选中其中一个词条, 然后添加一个强化档位
-                const sub = ruletka(this.sub);
-                const rank = get_random_int(0, 4);
-                sub.upgrade_ranks.push(rank);
-
-                // 通知事件
-                this.on_sub_stat_upgraded.emit({sub_stat_idx: this.sub.findIndex(x => x.sub_stat === sub.sub_stat && x.upgrade_ranks === sub.upgrade_ranks), rank});
-            }
-        }
-
-    }
-
-    /**
-     * 取当前圣遗物强化等级
-     */
-    public current_level = () => this.level;
-
-    /**
-     * 取当前圣遗物副词条
-     */
-    public get_sub_stat = () => this.sub;
-
-    /**
-     * 取当前圣遗物部位类型
-     */
-    public get_artifact_type = () => this.type;
-
-    /**
-     * 取当前圣遗物主词条
-     */
-    public get_main_type = () => this.main;
+type Artifact = {
+    OnArtifactLevelUpgraded(): Event<ArtifactLevelUpgradedEventData, undefined>;
+    OnArtifactSubStatAddedOrUpgraded(): Event<ArtifactSubStatAddedEventData, undefined>;
+    LevelUpgrade(level: number): void;
+    CurrentLevel(): number;
+    GetMainStat(): Stat
 }
+
+type ArtifactLevelUpgradedEventData = {
+    currentLevel: number
+}
+
+type ArtifactSubStatAddedEventData = {
+    subStat: Stat,
+    upgradeRank: number
+}
+
+export function NewArtifact(artifactType: ArtifactType): Artifact {
+    // 圣遗物升级事件
+    let onArtifactLevelUpgraded: Event<ArtifactLevelUpgradedEventData, undefined>;
+    // 副词条被添加/强化事件
+    let onArtifactSubStatAddedOrUpgraded: Event<ArtifactSubStatAddedEventData, undefined>;
+
+    // 当前强化等级
+    let currentLevel: number;
+
+    // 主词条
+    let mainStat: Stat;
+
+    // 副词条集合
+    let subStats: { subStat: Stat, rank: number[] }[];
+
+    // 为圣遗物添加一个副词条
+    const addNewSubStat = ():{ subStat: Stat, rank: number[]} => {
+        // 已有词条集合
+        const subStatsArray = subStats.map(x => {
+            return x.subStat;
+        });
+
+        const newSubStatObject = {
+            // 选出一个随机词条
+            subStat: GetNewSubStat(subStatsArray),
+            // 为词条选择一个初始的强化档位
+            rank: [GetRandom(ValueType.Int, 0, 4)],
+        };
+
+        // 插入词条
+        subStats.push(newSubStatObject);
+
+        // 返回该次选出的新词条
+        return { ...newSubStatObject };
+    }
+
+    // 初始化闭包
+    (()=> {
+        // 初始化事件
+        onArtifactLevelUpgraded = new Event<ArtifactLevelUpgradedEventData, undefined>();
+        onArtifactSubStatAddedOrUpgraded = new Event<ArtifactSubStatAddedEventData, undefined>();
+
+        // 设置初始强化等级
+        currentLevel = 0;
+        // 随机选取主词条
+        mainStat = GetRandomMainStatByArtifactType(artifactType);
+
+        // 随机决定初始副词条的数量
+        const c = GetRandom(ValueType.Int, 0, 2000) % 2 === 0 ? 3 : 4;
+
+        // 添加副词条
+        for(let i = 0; i < c; i++) {
+            const result = addNewSubStat();
+            // 触发词条添加事件
+            onArtifactSubStatAddedOrUpgraded.emit({
+                subStat: result.subStat,
+                upgradeRank: result.rank[0]
+            })
+        }
+
+
+    })();
+
+
+    return {
+        OnArtifactLevelUpgraded(): Event<ArtifactLevelUpgradedEventData, undefined> {
+            return onArtifactLevelUpgraded;
+        },
+        OnArtifactSubStatAddedOrUpgraded(): Event<ArtifactSubStatAddedEventData, undefined> {
+            return onArtifactSubStatAddedOrUpgraded;
+        },
+        LevelUpgrade(): void {
+            if(currentLevel < 0){
+                throw new Error(`Artifact level cannot less than 0, maybe an error occurred during init.`);
+            }
+
+            if(currentLevel >= 20){
+                throw new Error(`Out of limit.`);
+            }
+
+            // 等级+1
+            currentLevel ++ ;
+
+            // 如果强化后的强化等级等于4 8 12 16 20时,
+            if(currentLevel == 4 || currentLevel == 8 || currentLevel == 12 || currentLevel == 16 || currentLevel == 20){
+                if(subStats.length < 4){
+                    // 且当前副词条数量少于4个时,
+                    // 添加一个随机词条
+                    const result = addNewSubStat();
+                    // 触发词条添加事件
+                    onArtifactSubStatAddedOrUpgraded.emit({
+                        subStat: result.subStat,
+                        upgradeRank: result.rank[0]
+                    })
+
+                } else {
+                    // 如果已经有4个词条了,
+                    // 选择其中一个词条, 添加一个强化档位
+                    const randomRank = GetRandom(ValueType.Int, 0, 4);
+                    const stat = Ruletka(subStats);
+                    stat.rank.push(randomRank);
+                    // 触发词条强化事件
+                    onArtifactSubStatAddedOrUpgraded.emit({
+                        subStat: stat.subStat,
+                        upgradeRank: randomRank
+                    })
+                }
+
+                // 触发圣遗物升级事件
+                onArtifactLevelUpgraded.emit({
+                    currentLevel: currentLevel
+                });
+
+            }
+
+
+
+        },
+        CurrentLevel(): number {
+            return currentLevel;
+        },
+        GetMainStat(): Stat {
+            return mainStat;
+        }
+
+    }
+
+
+
+}
+
--- src/event.ts
+++ src/event.ts
@@ -1,24 +1,24 @@
 /**
  * 事件回调
  */
-export type EventHandle<T> = (data: T, e?: any) => any;
+export type EventHandle<T1, T2> = (data: T1, e?: T2) => void;
 
 /**
  * 事件
  */
-export class Event<T> {
+export class Event<T1, T2> {
     /**
      * 事件回调数组
      * @private
      */
-    private handles: EventHandle<T>[] = [];
+    private handles: EventHandle<T1, T2>[] = [];
 
     /**
      * 订阅事件
      *
      * @param handle
      */
-    public subscribe(handle: EventHandle<T>) {
+    public subscribe(handle: EventHandle<T1, T2>) {
         this.handles.push(handle);
 
     }
@@ -28,7 +28,7 @@ export class Event<T> {
      *
      * @param handle
      */
-    public unsubscribe(handle: EventHandle<T>) {
+    public unsubscribe(handle: EventHandle<T1, T2>) {
         this.handles = this.handles.filter(h => h !== handle);
     }
 
@@ -47,7 +47,7 @@ export class Event<T> {
      * @param {T} data 通知信息
      * @param {any?} e 事件附加内容
      */
-    public emit(data: T, e?: any){
+    public emit(data: T1, e?: T2){
         for(let i = 0; i < this.handles.length; i++){
             const fn = this.handles[i];
 
